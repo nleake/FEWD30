f6adfcd86191da063fc48eb23130d98235814c94
Nick Leake <leakend@dukes.jmu.edu>
2017-03-01 22:34:41 -0500

    added background switching to temperature converter

--- JS/convert-temperature/js/index.js
+++ JS/convert-temperature/js/index.js
@@ -4,9 +4,45 @@ $('#convert').click(convert);
 
 
 function convert(a) {
+  //clear classes each time we click
+  removeAllClasses('body');
   a = $('#a').val();
   var b = Math.floor((a - 32) * (5/9));
   console.log(a);
   console.log(b);
-  $('#b').html(b + "&deg celcius");
-}//end convert
+  $('#b').html(b + "&deg celsius");
+
+  //temperature background logic
+  if (b > 25) {// 78 F or greater
+    $('body').toggleClass('summer');
+  } else if (b > 12) {
+    $('body').toggleClass('spring');
+  } else if (b >= 0) {
+    $('body').toggleClass('autumn');
+  } else if (b  < 0) {//less than 40 F
+    $('body').toggleClass('winter');
+  }
+
+}// end convert
+
+function removeAllClasses(target){
+  $(target).removeClass();
+}
+
+// switch statement that we're not going to use
+
+// function evaluateTemp(temp) {
+//     switch(temp) {
+//       case (temp > 26):
+//         $('body').toggleClass('summer');
+//         break;
+//       case (temp > 10):
+//         $('body').toggleClass('spring');
+//         break;
+//       case (temp > 4):
+//         $('body').toggleClass('autumn');
+//         break;
+//       case (temp < 4):
+//         $('body').toggleClass('winter');
+//         break;
+// }
