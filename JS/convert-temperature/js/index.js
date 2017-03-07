ab39252d3bdcb316efb28d39a88a657520ce3062
Nick Leake <leakend@dukes.jmu.edu>
2017-03-06 22:51:35 -0500

    updated for refactoring and debugging exercises

--- JS/convert-temperature/js/index.js
+++ JS/convert-temperature/js/index.js
@@ -11,14 +11,24 @@ function convert(a) {
   console.log(a);
   console.log(b);
   $('#b').html(b + "&deg celsius");
+  $('#shading').css({
+      "color": "white",
+      "font-weight": "bold",
+      "background": "rgba(0, 0, 0, .5)"
+    });
 
   //temperature background logic
   if (b > 25) {// 78 F or greater
     $('body').toggleClass('summer');
   } else if (b > 12) {
     $('body').toggleClass('spring');
+    $('#inputs p').css({
+      "color": "white",
+      "font-weight": "bold"
+    });
   } else if (b >= 0) {
     $('body').toggleClass('autumn');
+
   } else if (b  < 0) {//less than 40 F
     $('body').toggleClass('winter');
   }
